49c529260b864d81f7471d7fc3e699b730fe1b32
Kyung Yeol Kim <kim.kyungyeol@croquis.com>
2021-04-19 03:49:29 +0900

    update zoom.

--- zoom/api/index.js
+++ zoom/api/index.js
@@ -2,6 +2,7 @@ const PERSONAL_ROOM = 'https://us05web.zoom.us/j/8935240452';
 
 module.exports = (req, res) => {
   const [shortPass] = req.url.split('?');
+  // https://zoom.chitacan.io/<ZOOM_SHORT_PWD>
   if (shortPass === `/${process.env.ZOOM_SHORT_PWD}`) {
     return res.redirect(308, `${PERSONAL_ROOM}?pwd=${process.env.ZOOM_PWD}`);
   }
